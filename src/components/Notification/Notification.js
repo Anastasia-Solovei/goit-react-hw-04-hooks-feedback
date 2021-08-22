065580efcaf7e610e97756b87aaec94c51998fab
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-08-22 14:30:19 +0300

    adds notification component and its styles

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import s from "./Notification.module.css";
+import PropTypes from "prop-types";
+
+const Notification = ({ message }) => {
+  return <p className={s.message}>{message}</p>;
+};
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+export default Notification;
